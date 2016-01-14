35effc0295108e5fe69f6d3b8912f5077ea4286d
jesusdaniel90 <usuario1dani@gmail.com>
2016-01-14 09:13:17 +0100

    Update App2.java

--- src/main/java/es/egc/App2.java
+++ src/main/java/es/egc/App2.java
@@ -10,7 +10,7 @@ public class App2
 	
 	public static int resta(int a, int b){
 		int result= a*b;
-		System.out.println("Returning "+a+"-"+b+"="+result);
+		System.out.println("Returning "+a+"*"+b+"="+result);
 		return result;
 	}
 }
